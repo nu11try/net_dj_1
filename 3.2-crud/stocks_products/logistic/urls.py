7d52c4c1d66d655684f94a72b80b7371643dc834
Zuev Vladislav <v.zuev@jump.finance>
2023-06-09 12:03:13 +0300

    файлы по домашней работе

--- 3.2-crud/stocks_products/logistic/urls.py
+++ 3.2-crud/stocks_products/logistic/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 
-from logistic.views import ProductViewSet, StockViewSet
+from .views import ProductViewSet, StockViewSet
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
